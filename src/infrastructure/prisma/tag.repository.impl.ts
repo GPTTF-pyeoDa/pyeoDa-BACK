f09f7cf36e156a1d783976992550f0ba3d09173f
choiyesong <cys826@jinhak.com>
2025-01-01 16:16:23 +0900

    refactor: tag save 메서드 에러처리

--- src/infrastructure/prisma/tag.repository.impl.ts
+++ src/infrastructure/prisma/tag.repository.impl.ts
@@ -8,12 +8,18 @@ export class TagRepositoryImpl implements TagRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async save(tag: Tag): Promise<void> {
-    await this.prisma.tag.create({
-      data: {
-        id: tag.id,
-        name: tag.name,
-        createdAt: tag.createdAt,
-      },
-    });
+    try {
+      await this.prisma.tag.create({
+        data: {
+          id: tag.id,
+          name: tag.name,
+          createdAt: tag.createdAt,
+        },
+      });
+    } catch (error) {
+      // 적절한 에러 로깅을 추가
+      console.error('Tag saving failed:', error);
+      throw new Error('Unable to save the tag');
+    }
   }
 }
